8802f8cdf769d3cc5e648137746d595dc2ccb74c
Blue-Onion <rawatadityasingh963@gmail.com>
2025-11-15 17:56:31 +0530

    feat:setting base of backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,2 @@
-import express from "express";
-
-const app = express();
-const port = process.env.PORT||3000;
-
-app.get("/", (req: express.Request, res: express.Response) => {
-  res.send("Hello World!");
-});
-app.get("/health", (req: express.Request, res: express.Response) => {
-  res.status(200).send("Ok api is working");
-});
-
+import "./server.ts";
+import dotenv from "dotenv";
